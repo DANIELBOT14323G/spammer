94af9743abc78ac32bf3e3feb0996ffed6081a23
daniel o s <93184223+DANIELBOT14323G@users.noreply.github.com>
2021-12-17 05:09:06 +0700

    Update Daniel.sh

--- Daniel.sh
+++ Daniel.sh
@@ -5,5 +5,24 @@
 figlet loading | lolcat
 sleep 1
 
-got clone DANIELBOT14323G/
+git clone DANIELBOT14323G/spamcalldata
+
+figlet selesai | lolcat
+sleep 1
+cd spamcalldata
+sleep 1
+
+# tampilan
+clear
+toilet -f big -F spamcall
+echo "==========================================">lolcat
+echo "(+) author : Daniel/mr redwhitehack8   (+)"
+echo "(+) team   : red white hack 8 team     (+)"
+echo "==========================================">lolcat
+echo
+echo " Gunakan dengan bijak "|lolcat
+echo "==========================================">lolcat
+echo
+python daniel.py
+
 
