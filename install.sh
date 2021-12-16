3c717726b3f067765854dd72a6c3db3b01f44952
daniel o s <93184223+DANIELBOT14323G@users.noreply.github.com>
2021-12-17 04:35:11 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+pkg update && pkg upgrade
+pkg install python
+pkg install figlet
+pkg install toilet
+pkg install ruby
+pkg install lolcat
+pkg install nano
+git clone https://github.com/DANIELBOT14323G/spammer
+bash install.sh
+
